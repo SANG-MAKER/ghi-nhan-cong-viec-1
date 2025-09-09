f9786dff2386a71d7743bf4b012a4901ae14a3ea
SANG-MAKER <sangnn@daidung.vn>
2025-09-09 11:42:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ if tasks:
         )
         st.plotly_chart(fig_stacked, use_container_width=True)
         
-  with st.expander("📊 Thống kê theo loại Công việc"):
+   with st.expander("📊 Thống kê theo loại Công việc"):
     type_chart = df["task_type"].value_counts().reset_index()
     type_chart.columns = ["Loại công việc", "Số lượng"]
     fig_type = px.bar(
